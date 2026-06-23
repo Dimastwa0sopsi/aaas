dd2e34b160c109f60911f543ad9989c1299dfd87
GimPrograms <dimastvincs@gmail.com>
2026-06-23 22:10:41 +0400

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-# Токен бота
-BOT_TOKEN = "8914142458:AAGdwsswgp7b0nN9f8OCyNs_IYMx2gQkK5Q"
-
-# ID владельца
-OWNER_ID = 8415068937
-
-# Ссылка на сайт
-SITE_URL = "https://dimastwa0sopsi.github.io/Banana/"
