867d807f875629d107b2fb4ba52795a2e3760ea9
GimPrograms <dimastvincs@gmail.com>
2026-06-23 22:10:23 +0400

    Delete states directory

--- states/support_states.py
+++
@@ -1,6 +0,0 @@
-from aiogram.fsm.state import State, StatesGroup
-
-class SupportStates(StatesGroup):
-    waiting_for_problem = State()
-    waiting_for_code = State()
-    waiting_for_dev_message = State()
