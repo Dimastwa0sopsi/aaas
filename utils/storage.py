e95840b7267059846c7a2eb255935d1fca8095ea
GimPrograms <dimastvincs@gmail.com>
2026-06-23 22:10:32 +0400

    Delete utils directory

--- utils/storage.py
+++
@@ -1,5 +0,0 @@
-# Словарь для хранения связи: message_id_владельца -> user_id
-support_requests = {}
-
-# Словарь для хранения кодов регистрации: код -> {"user_id": int, "nickname": str}
-pending_codes = {}
